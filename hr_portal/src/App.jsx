06c5e06da58d2e3b61d1757c1a5a0f3c7e0cf66a
Sumedh Zodape <sumedh@vitric.in>
2024-07-11 14:23:54 +0530

    routes

--- hr_portal/src/App.jsx
+++ hr_portal/src/App.jsx
@@ -1,30 +1,66 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar/sidebar";
 import Navbar from "./Components/Navbar/Navbar";
-import Dashboard from './Components/Dashboard/Dashboard';
-import { useState } from 'react'
-import './App.css'
-import Login from './Components/Login.jsx';
-import EmployeesTable from './Components/EmployeesTable.jsx';
-import AddCandidate from './Components/AddCandidate.jsx';
-import ClientList from './Components/ClientList.jsx';
-import ClientView from './Components/ClientView.jsx';
-
-
-
-import './App.css';
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Login from "./Components/Login.jsx";
+import EmployeesTable from "./Components/EmployeesTable.jsx";
+import AddCandidate from "./Components/AddCandidate.jsx";
+import ClientList from "./Components/ClientList.jsx";
+import ClientView from "./Components/ClientView.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <>
-       <div className="app-container">
+    <BrowserRouter>
+      <div className="app-container">
         <Sidebar />
         <div className="main-content">
-        <Navbar />
-        <Dashboard />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/employees" element={<EmployeesTable />} />
+            <Route path="/candidates" element={<AddCandidate />} />
+            <Route path="/clients" element={<ClientList />} />
+            {/* <Route path="/client-view/:id" element={<ClientView />} /> */}
+          </Routes>
+        </div>
       </div>
-    </div>
-    </>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+// import Sidebar from "./Components/Sidebar/sidebar";
+// import Navbar from "./Components/Navbar/Navbar";
+// import Dashboard from './Components/Dashboard/Dashboard';
+// import { useState } from 'react'
+// import './App.css'
+// import Login from './Components/Login.jsx';
+// import EmployeesTable from './Components/EmployeesTable.jsx';
+// import AddCandidate from './Components/AddCandidate.jsx';
+// import ClientList from './Components/ClientList.jsx';
+// import ClientView from './Components/ClientView.jsx';
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <>
+//        <div className="app-container">
+//         <Sidebar />
+//         <div className="main-content">
+//         <Navbar />
+//         <Dashboard />
+//       </div>
+
+//     </div>
+//     {/* <EmployeesTable/> */}
+//     </>
+//   );
+// }
+
+// export default App;
--- hr_portal/src/Components/Sidebar/SidebarLinks/SidebarLinks.jsx
+++ hr_portal/src/Components/Sidebar/SidebarLinks/SidebarLinks.jsx
@@ -6,61 +6,53 @@ import { MdSchedule } from "react-icons/md";
 import { FaUserFriends } from "react-icons/fa";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBriefcase , faGear } from '@fortawesome/free-solid-svg-icons';
-// import { FiSettings } from "react-icons/fi";
 import { TbLayoutDashboardFilled } from "react-icons/tb";
 import { MdOutlineAccessTimeFilled } from "react-icons/md";
 import { FaBriefcase } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 import Logo from '../logo/logo';
-
 
 const SidebarLinks = () => {
   return (
-
-    <div className='SideBar_Links' >
-    <Logo />
-    <div className="nav_links ">
-        <div className='link active '>
-            <TbLayoutDashboardFilled  className='w-6 h-6 text-dark cursor-pointer'/>
-            <a href="" >Dashboard</a>
+    <div className='SideBar_Links'>
+      <Logo />
+      <div className="nav_links">
+        <div className='link active'>
+          <TbLayoutDashboardFilled className='w-6 h-6 text-dark cursor-pointer'/>
+          <Link to="/">Dashboard</Link>
         </div>
 
         <div className='link'>
-            <IoIosPeople  className='w-6 h-6 text-dark cursor-pointer'/>
-            <a href="">All Employees</a>
+          <IoIosPeople className='w-6 h-6 text-dark cursor-pointer'/>
+          <Link to="/employees">All Employees</Link>
         </div>
 
         <div className='link'>
-            {/* <MdSchedule  className='w-6 h-6 text-dark cursor-pointer'/> */}
-            <MdOutlineAccessTimeFilled   className='w-6 h-6 text-dark cursor-pointer'/>
-            <a href="">Schedule</a>
+          <MdOutlineAccessTimeFilled className='w-6 h-6 text-dark cursor-pointer'/>
+          <Link to="/schedule">Schedule</Link>
         </div>
 
         <div className='link'>
-        {/* <FaBriefcase className='w-6 h-6 text-dark cursor-pointer'/> */}
-        <FaBriefcase className='w-5 h-5 text-dark cursor-pointer'/>
-        <a href="">Jobs</a>
+          <FaBriefcase className='w-5 h-5 text-dark cursor-pointer'/>
+          <Link to="/jobs">Jobs</Link>
+        </div>
+
+        <div className='link'>
+          <FaUserFriends className='w-6 h-6 text-dark cursor-pointer'/>
+          <Link to="/candidates">Candidates</Link>
+        </div>
+
+        <div className='link'>
+          <LuLayoutDashboard className='w-6 h-6 text-dark cursor-pointer'/>
+          <Link to="/clients">Clients</Link>
+        </div>
+
+        <div className='link'>
+          <FontAwesomeIcon icon={faGear} className='w-6 h-6 text-dark cursor-pointer' />
+          <Link to="/settings">Settings</Link>
+        </div>
       </div>
-
-        <div className='link'>
-            <FaUserFriends  className='w-6 h-6 text-dark cursor-pointer'/>
-            <a href="">Candidates</a>
-        </div>
-        <div className='link'>
-            <LuLayoutDashboard  className='w-6 h-6 text-dark cursor-pointer'/>
-            <a href="">Clients</a>
-        </div>
-        <div className='link'>
-            {/* <FiSettings  className='w-6 h-6 text-dark cursor-pointer'/> */}
-            <FontAwesomeIcon icon={faGear} className='w-6 h-6 text-dark cursor-pointer' />
-            <a href="">Settings</a>
-        </div>
-   </div>
-    
-    
-    
     </div>
-
-
   )
 }
 
