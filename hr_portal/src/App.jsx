e57cec66acd2c1e1f4dee1116986427c445be420
shachi <shachi.mandavgane16@gmail.com>
2024-07-11 14:54:34 +0530

    updates in App jsx file

--- hr_portal/src/App.jsx
+++ hr_portal/src/App.jsx
@@ -1,28 +1,28 @@
 import Sidebar from "./Components/Sidebar/sidebar";
 import Navbar from "./Components/Navbar/Navbar";
-import Dashboard from './Components/Dashboard/Dashboard';
-import { useState } from 'react'
-import './App.css'
-import Login from './Components/Login.jsx';
-import EmployeesTable from './Components/EmployeesTable.jsx';
-import AddCandidate from './Components/Candidate/AddCandidate.jsx';
-import ClientList from './Components/ClientsComponent/ClientList.jsx';
-import ClientView from './Components/ClientsComponent/ClientView.jsx';
+import Dashboard from "./Components/Dashboard/Dashboard";
+import { useState } from "react";
+import "./App.css";
+import Login from "./Components/Login.jsx";
+import EmployeesTable from "./Components/EmployeesTable.jsx";
+import AddCandidate from "./Components/Candidate/AddCandidate.jsx";
+import ClientList from "./Components/ClientsComponent/ClientList.jsx";
+import ClientView from "./Components/ClientsComponent/ClientView.jsx";
 
-
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-       <div className="app-container">
-        <Sidebar />
+      <div className="app-container">
+        {/* <Sidebar />
         <div className="main-content">
         <Navbar />
         <Dashboard />
+      </div> */}
+
+        <Login />
       </div>
-    </div>
     </>
   );
 }
